5e302dd2be8aab07a8c7e5315db57f24f08421e5
praneash-cdw <praneash.krishnamurthi@cdw.com>
2024-09-12 23:43:19 +0530

    added counter logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-// import HandAnimation from "@/components/handAnimation/HandAnimation";
-// import JumpAnimation from "@/components/jumpingAnimation/JumpingAnimation";
-
 import { CountDown } from '@/components/countDown/CountDown';
 
 export default function Home() {
   return (
     <>
-      {/* <HandAnimation /> */}
-      {/* <JumpAnimation/> */}
-
       <CountDown></CountDown>
     </>
   );
