d53b12e16c9bcc5e1587b65cb3430b9445cfc2e2
Laxmanapandi Baskaran <laxmanapandi.baskaran@cdw.com>
2024-09-21 19:38:46 +0530

    Dummy Fullpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,48 @@
-import CountDown from '@/components/countDown/CountDown';
-import Logo from '@/components/Logo/Logo';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+'use client';
 import BackgroundAnimation from '@/components/BackgroundAnimation/BackgroundAnimation';
 import LandscapeFallback from '@/components/LandscapeFallback/LandscapeFallback';
 
+import ReactFullpage from '@fullpage/react-fullpage';
+
 export default function Home() {
   return (
-    <div className="home-page h-screen w-screen">
-      <div className="home">
-        <BackgroundAnimation />
-        <Logo />
-        <CountDown />
-      </div>
-      <div className="landscape-fallback-page justify-center min-h-screen items-center">
-        <BackgroundAnimation />
-        <LandscapeFallback></LandscapeFallback>
-      </div>
-    </div>
+    // <div className="home-page h-screen w-screen">
+    //   <div className="home">
+    //     <BackgroundAnimation />
+    //     <SideNavigation />
+    //   </div>
+    //   <div className="landscape-fallback-page justify-center min-h-screen items-center">
+    //     <BackgroundAnimation />
+    //     <LandscapeFallback></LandscapeFallback>
+    //   </div>
+    // </div>
+    <ReactFullpage
+      navigation
+      // scrollHorizontally = {true}
+      sectionsColor={[
+        'blue',
+        'red',
+        'orange',
+        '#435b71',
+        'orange',
+        'blue',
+        'purple',
+        'yellow',
+      ]}
+      render={() => (
+        <ReactFullpage.Wrapper>
+          <div key={'section1'} className="section">
+            <h1>section1</h1>
+          </div>
+          <div key={'section2'} className="section">
+            <h1>section2</h1>
+          </div>
+          <div key={'section3'} className="section">
+            <h1>section3</h1>
+          </div>
+        </ReactFullpage.Wrapper>
+      )}
+    />
   );
 }
