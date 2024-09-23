dd04ea0ed0efb732aee6a6afac03f6c04a976e73
praneash-cdw <praneash.krishnamurthi@cdw.com>
2024-09-23 18:52:35 +0530

    added constant

--- src/components/BackGround/BackGround.tsx
+++ src/components/BackGround/BackGround.tsx
@@ -2,6 +2,7 @@
 import { ReactNode, useEffect, useRef, useState } from 'react';
 import WAVES from 'vanta/dist/vanta.waves.min.js';
 import * as THREE from 'three';
+import { BACKGROUND_VANTA_SETTINGS } from '@/constants/constants';
 interface BackGroundProps {
   children: ReactNode;
 }
@@ -14,18 +15,18 @@ export default function BackGround({ children }: BackGroundProps) {
         WAVES({
           el: vantaRef.current,
           THREE,
-          mouseControls: true,
-          touchControls: true,
-          gyroControls: false,
-          minHeight: 200.0,
-          minWidth: 200.0,
-          scale: 1.0,
-          scaleMobile: 1.0,
-          color: 0x270212,
-          shininess: 13.0,
-          waveHeight: 4.5,
-          waveSpeed: 1.2,
-          zoom: 0.96,
+          mouseControls: BACKGROUND_VANTA_SETTINGS.MOUSECONTROLS,
+          touchControls: BACKGROUND_VANTA_SETTINGS.TOUCHCONTROLS,
+          gyroControls: BACKGROUND_VANTA_SETTINGS.GYROCONTROLS,
+          minHeight: BACKGROUND_VANTA_SETTINGS.MINHEIGHT,
+          minWidth: BACKGROUND_VANTA_SETTINGS.MINWIDTH,
+          scale: BACKGROUND_VANTA_SETTINGS.SCALE,
+          scaleMobile: BACKGROUND_VANTA_SETTINGS.SCALEMOBILE,
+          color: BACKGROUND_VANTA_SETTINGS.COLOR,
+          shininess: BACKGROUND_VANTA_SETTINGS.SHININESS,
+          waveHeight: BACKGROUND_VANTA_SETTINGS.WAVEHEIGHT,
+          waveSpeed: BACKGROUND_VANTA_SETTINGS.WAVESPEED,
+          zoom: BACKGROUND_VANTA_SETTINGS.ZOOM,
         }),
       );
     }
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -19,3 +19,17 @@ export const ALLSPARK_DATE = 'October 25 & 26, Bengaluru';
 
 export const MOBILE_LANDSCAPE_MESSAGE =
   'Please rotate your device to portrait mode';
+export const BACKGROUND_VANTA_SETTINGS = {
+  MOUSECONTROLS: true,
+  TOUCHCONTROLS: true,
+  GYROCONTROLS: false,
+  MINHEIGHT: 200.0,
+  MINWIDTH: 200.0,
+  SCALE: 1.0,
+  SCALEMOBILE: 1.0,
+  COLOR: 0x270212,
+  SHININESS: 13.0,
+  WAVEHEIGHT: 4.5,
+  WAVESPEED: 1.2,
+  ZOOM: 0.96,
+};
