32fc8241e09d7a208412e725189bf982e87a8e82
Swetha Prakashan <swetha.prakashan@cdw.com>
2024-09-25 19:12:21 +0530

    CDW logo added to footer

--- src/containers/CommitteeContainer/CommitteeContainer.tsx
+++ src/containers/CommitteeContainer/CommitteeContainer.tsx
@@ -1,6 +1,8 @@
 import ComingSoon from '@/components/ComingSoon/ComingSoon';
 import Header from '@/components/Header/Header';
 import React from 'react';
+import Image from 'next/image';
+import cdwlogo from '../../../public/images/cdwLogo.png';
 
 const CommitteeContainer = () => {
   return (
@@ -11,6 +13,15 @@ const CommitteeContainer = () => {
         className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
       >
         <ComingSoon />
+      </div>
+      <div className="footer-logo p-10 absolute bottom-0 left-1/2 transform -translate-x-1/2 flex justify-center ">
+        <Image
+          src={cdwlogo}
+          alt="cdw-logo"
+          className="object-contain"
+          width={105}
+          height={56}
+        />
       </div>
     </div>
   );
