6e80048bcb3233680e8dc09fb96868605b1a7c34
Hariharan482 <125631751+Hariharan482@users.noreply.github.com>
2024-09-26 19:27:07 +0530

    small mobile style fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,13 +11,15 @@ import cdwlogo from '../../../public/images/cdwLogo.png';
 const Header = () => {
   return (
     <div className="flex justify-center md:justify-between px-12 py-10 fixed top-0 z-50 w-screen">
-      <Image
-        src={allspark}
-        alt="allspark-logo"
-        className="object-contain"
-        width={263}
-        height={73}
-      />
+      <div className="flex items-center bg-[#27021295] justify-center w-[300px] h-[92px] rounded-[50px] p-4">
+        <Image
+          src={allspark}
+          alt="allspark-logo"
+          className="object-contain"
+          width={263}
+          height={73}
+        />
+      </div>
       <Image
         src={cdwlogo}
         alt="cdw-logo"
