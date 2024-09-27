6c2f5895203e51c04e37f36064c5927c313c3ddf
rubeshudayakumar <rubesh.udayakumar@cdw.com>
2024-09-27 15:10:47 +0530

    video header removal and video resizing

--- src/containers/AboutContainer/AboutContainer.tsx
+++ src/containers/AboutContainer/AboutContainer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import SectionHeader from '@/components/SectionHeader/SectionHeader';
+// import SectionHeader from '@/components/SectionHeader/SectionHeader';
 import VideoPlayer from '@/components/VideoPlayer/VideoPlayer';
 import './AboutContainer.scss';
 
@@ -7,7 +7,7 @@ const AboutContainer = () => {
   return (
     <div className="h-screen relative">
       <div id="about-section" className="pt-[18vh]">
-        <SectionHeader headerText="About AllSpark'24" />
+        {/* <SectionHeader headerText="About AllSpark'24" /> */}
         <VideoPlayer
           videoUrl={
             'https://www.youtube.com/embed/YrYivVC-Yhg?si=OdvhFYA9PWtAfBha?rel=0'
