b5fa0c8927d6abdeb871db1a5d236ea7725bf6cb
Laxmanapandi Baskaran <laxmanapandi.baskaran@cdw.com>
2024-09-27 10:18:16 +0530

    Updating header stylings

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,21 +11,21 @@ import cdwlogo from '../../../public/images/cdwLogo.png';
 const Header = () => {
   return (
     <div className="flex justify-center md:justify-between px-12 py-10 fixed top-0 z-50 w-screen">
-      <div className="flex items-center bg-header-background justify-center w-[300px] h-[92px] rounded-[50px] p-4">
+      <div className="flex items-center bg-header-background justify-center w-[280px] h-[85px] rounded-[50px] p-4">
         <Image
           src={allspark}
           alt="allspark-logo"
           className="object-contain"
-          width={263}
-          height={73}
+          width={220}
+          height={60}
         />
       </div>
       <Image
         src={cdwlogo}
         alt="cdw-logo"
         className="object-contain hidden md:block"
-        width={105}
-        height={56}
+        width={90}
+        height={50}
       />
     </div>
   );
