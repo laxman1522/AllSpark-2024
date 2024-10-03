ae9d1630a73d824bee388b7e33b88e1cd1adf187
Charanrajcdw <charanraj.thiyagarajan@cdw.com>
2024-10-03 20:07:39 +0530

    splide config files added

---
+++ src/components/SliderNav/SliderNav.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+interface SliderNavProps {
+  prevSlide: () => void;
+  nextSlide: () => void;
+  currentIndex: number;
+  items: number;
+}
+
+export default function SliderNav({
+  prevSlide,
+  nextSlide,
+  currentIndex,
+  items,
+}: SliderNavProps) {
+  return (
+    <div className="flex justify-center items-center ">
+      <button onClick={prevSlide} className="p-1  bg-text-color rounded-full">
+        <ChevronLeft
+          size={24}
+          strokeWidth={4}
+          className="text-counter-background"
+        />
+      </button>
+
+      <span className="text-lg min-w-24 font-medium text-button-color text-center">
+        {currentIndex + 1} / {items}
+      </span>
+
+      <button onClick={nextSlide} className="p-1  bg-text-color rounded-full">
+        <ChevronRight
+          size={24}
+          strokeWidth={4}
+          className="text-counter-background"
+        />
+      </button>
+    </div>
+  );
+}
