6ed3c94eaf66baa0a8e56ed1fee457a3c6a18d5a
rubeshudayakumar <rubesh.udayakumar@cdw.com>
2024-10-10 11:57:07 +0530

    scroll fix

--- src/components/CommitteeSection/CommitteeSection.tsx
+++ src/components/CommitteeSection/CommitteeSection.tsx
@@ -14,24 +14,26 @@ const CommitteeSection: React.FC<CommitteeSectionProps> = ({ committee }) => {
   const committeeMembersAndNames = getCommitteeAndMembersNames();
 
   return (
-    <div className="members-grid relative w-[78%] h-[100%] flex gap-4 px-[60px] pt-[100px]">
-      <div className="m-auto relative top-[-33vh] section-header-wrapper px-12 flex items-center font-bold text-[35px] md:text-[28px] text-[#f3baa7] gap-5">
+    <div className="relative w-[78%] h-[100%] flex flex-col gap-4 px-[60px] pt-[50px]">
+      <div className="m-auto section-header-wrapper px-12 flex items-center font-bold text-[35px] md:text-[28px] text-[#f3baa7] gap-5">
         <span className="header-line h-[0.4vh] bg-[#F3BAA7] w-[4.5vw] max-[767px]:w-[20vw] max-[767px]:h-[4px]"></span>
         <h3 className="whitespace-nowrap text-[24px] max-[1180px]:text-[18px] max-[1180px]:font-medium text-button-color">
           {committee}
         </h3>
         <span className="header-line h-[0.4vh] bg-[#F3BAA7] w-[4.5vw] max-[767px]:w-[20vw] max-[767px]:h-[4px]"></span>
       </div>
-      {committeeMembersAndNames.map((member, index) => (
-        <div
-          key={index}
-          className={`element-item ${member.committeeName} absolute left-1/2`}
-          data-category={member.committeeName}
-        >
-          {/*  */}
-          <CommitteeMemberCard memberName={member.memberName} />
-        </div>
-      ))}
+      <div className="members-grid w-[100%]">
+        {committeeMembersAndNames.map((member, index) => (
+          <div
+            key={index}
+            className={`element-item ${member.committeeName} absolute left-1/2`}
+            data-category={member.committeeName}
+          >
+            {/*  */}
+            <CommitteeMemberCard memberName={member.memberName} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
