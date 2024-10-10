5c95a5d63db068bfd33bf82506f0af5d8b1ca554
ashok-cdw <ashok.natarajan@cdw.com>
2024-10-11 00:38:33 +0530

    Completed Feature Committee Mobile Component

---
+++ src/components/CommitteeSectionMobile/CommitteSectionMobile.tsx
@@ -0,0 +1,50 @@
+import CommitteeMemberCard from '../CommitteeMemberCard/CommitteeMemberCard';
+import './CommitteeSectionMobile.scss';
+
+interface CommitteeSectionMobileProps {
+  committeeAndMembersList: Array<Committee>;
+  committee: string;
+}
+
+interface Committee {
+  committeeName: string;
+  memberName: string;
+}
+
+/**
+ * @description A component for committee section mobile view with a list of members
+ * @version 1.0.0
+ * @author [Ashok Natarajan]
+ */
+const CommitteeSectionMobile: React.FC<CommitteeSectionMobileProps> = ({
+  committeeAndMembersList,
+  committee,
+}) => {
+  return (
+    <div className="rounded-lg overflow-hidden border w-[80vw] h-[60vh] m-auto mt-4 bg-events-counter-text-color border-memories-border-color">
+      <div className="px-8 max-[767px]:h-[5vh] max-[767px]:mt-[2vh] flex max-[767px]:justify-center items-center font-bold text-[35px] max-[767px]:text-[20px] md:text-[28px] text-[#f3baa7] max-[767px]:mb-[1vh] gap-5 max-[767px]:gap-2 committee-section-header">
+        <span className="h-[0.5vh] bg-memories-border-color w-[4.5vw] max-[767px]:w-[25vw] max-[767px]:h-[4px]"></span>
+        <h3 className="whitespace-wrap text-[36px] max-[580px]:text-[16px] text-center text-events-color">
+          {committee}
+        </h3>
+        <span className="h-[0.5vh] bg-memories-border-color w-[15vw] max-[767px]:w-[25vw] max-[767px]:h-[4px]"></span>
+      </div>
+      <div className="members-grid h-full">
+        {committeeAndMembersList.map((committee: Committee, index: number) => {
+          return (
+            <div
+              className={`element-item w-[50%] m-auto mt-4 overflow-y-scroll pb-4 committee-section-mobile ${committee.committeeName}`}
+              data-category={committee.committeeName}
+            >
+              <div className="flex justify-center items-center flex-wrap gap-4 mt-[-15px]">
+                <CommitteeMemberCard memberName={committee.memberName} />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default CommitteeSectionMobile;
--- src/containers/CommitteeContainer/CommitteeContainer.tsx
+++ src/containers/CommitteeContainer/CommitteeContainer.tsx
@@ -1,6 +1,9 @@
 import { useWindowSize } from '@/contexts/WindowSizeContext';
 import Committee from '@/components/Committee/Committee';
 import SectionHeader from '@/components/SectionHeader/SectionHeader';
+import { COMMITTEE } from '@/constants/constants';
+import CommitteeMobile from '@/components/CommitteeMobile/CommitteeMobile';
+import './CommitteeContainer.scss';
 
 const CommitteeContainer = () => {
   const { windowWidth } = useWindowSize();
@@ -9,7 +12,10 @@ const CommitteeContainer = () => {
     <div className="h-screen relative">
       <div id="committee-section">
         {windowWidth < 1024 ? (
-          <div> MEMORIES MOBILE </div>
+          <div className="mt-28 w-full h-full inline-block committee-container-mobile">
+            <SectionHeader headerText={COMMITTEE.committee} />
+            <CommitteeMobile />
+          </div>
         ) : (
           <div className="ml-[4vw] pt-[150px]">
             <SectionHeader headerText="Committee" />
